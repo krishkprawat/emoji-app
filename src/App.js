814e8959aa074b6f00ccbdbc3a432e4f2dc4a0ab
krishnapal singh rawat <47602022+krishkprawat@users.noreply.github.com>
2020-12-14 18:25:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "😘": "Blow A Kiss",
+  "😠": "Angry",
+  "🤣": "ROFL",
+  "😉": "Wink Face",
+  "🤐": "Zip It",
+  "🤭": "Face with Hand Over Mouth",
+  "🤭": "Face with Hand Over Mouth",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "eutral Face",
+  "😑": "Expressionless Face",
+  "😏": " Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤤": "Drooling Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": " Face with Thermometer",
+  "🤕": " Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🤠": "Cowboy Hat Face",
+  "🥳": "Partying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "🤓": "Nerd Face",
+  "🤏": "Pinching Hand",
+  "✌️": "Victory Hand",
+  "🤞": "Crossed Fingers",
+  "🤟": "Love-You Gesture",
+  "🤘": "Sign of the Horns",
+  "🤙": "Call Me Hand",
+  "👈": "Backhand Index Pointing Left",
+  "👉": "Backhand Index Pointing Right",
+  "👆": "Backhand Index Pointing Up",
+  "🖕": "Middle Finger",
+  "👇": "Backhand Index Pointing Down",
+  "☝️": "Index Pointing Up",
+  "👍": "Thumbs Up",
+  "👎": "Thumbs Down",
+  "✊": "Raised Fist",
+  "👊": "Oncoming Fist",
+  "🤛": "Left-Facing Fist",
+  "🤜": "Right-Facing Fist",
+  "👏": "Clapping Hands",
+  "🙌": "Raising Hands",
+  "👐": "Open Hands",
+  "🤲": "Palms Up Together",
+  "🤝": "Handshake",
+  "🙏": "Folded Hands",
+  "💅": "Nail Polish",
+  "🤳": "Selfie",
+  "💪": "Flexed Biceps",
+  "🦾": "Mechanical Arm",
+  "🦿": "Mechanical Leg",
+  "🦵": "Leg",
+  "🦶": "Foot",
+  "👦": "Boy",
+  "👧": "Girl",
+  "🧑": "Person"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("meaning of emoji appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("oops.. this emoji is not recognized.");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    /** concept 3 is onchange */
+    <div className="App">
+      <h1>Inside Outtttt</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Search your emoji here:"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2>
+      <h3> {meaning} </h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
+    </div>
+  );
+}
